3cd6d23e6c17bc719413d86d5fdbc9bb0bb46d25
John B. Lowe <jblowe@berkeley.edu>
2026-07-14 11:12:09 -0700

    revised pipeline script update_mmap.sh

--- mmap-solr/update_mmap.sh
+++ mmap-solr/update_mmap.sh
@@ -7,17 +7,17 @@ source /home/ubuntu/.profile
 cd /home/ubuntu/mmap-code/mmap-solr/
 
 # update derivatives
-time ./make_derivatives.sh ${PHOTO_DIR}/originals ${PHOTO_DIR}/derivatives >> mmap-derivatives.txt 2>&1
+time ./pipeline/build_derivatives.sh ${PHOTO_DIR}/originals ${PHOTO_DIR}/derivatives >> mmap-derivatives.txt 2>&1
 
 # concatenate all tablet GIS files together and process into postgres
 cat /mnt/images/MMAP_GIS_data/* > csvtoload.csv
-time ./loadpostgres.sh csvtoload.csv "$CONNECT_STRING" mapping-tablet-to-postgres.csv >> mmap-loadpostgres.txt 2>&1
+time ./pipeline/load_tablet_gis.sh csvtoload.csv "$CONNECT_STRING" pipeline/tablet_to_postgres_columns.csv >> mmap-loadpostgres.txt 2>&1
 
 # reload artifacts solr core
-time ./solrETL-artifacts.sh mmap >> mmap-reload-artifacts.txt 2>&1
+time ./pipeline/reload_artifacts_core.sh mmap >> mmap-reload-artifacts.txt 2>&1
 
 # reload sites solr core
-time ./reload_sites.sh ${PHOTO_DIR}/derivatives >> mmap-reload-sites.txt 2>&1
+time ./pipeline/reload_sites_core.sh ${PHOTO_DIR}/derivatives >> mmap-reload-sites.txt 2>&1
 
 # regenerate site catalog, but leave it in the runtime directory
-python make_report.py mmap-sites.csv aws > site_report.html
+python pipeline/generate_site_catalog.py mmap-sites.csv aws > site_report.html
